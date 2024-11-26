6d1bb9ae922a087de4635b611fa1b5451d1c2184
Matias Sebastian Chocobar <111932821+ChocobarMatias@users.noreply.github.com>
2024-11-26 04:51:23 -0300

    correccion tp 8

--- ejercicio.py
+++ ejercicio.py
@@ -3,53 +3,53 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def Venta_Graficar(datos_producto, producto):
-    ventas_por_producto = agrupar_ventas(datos_producto)
+def Venta_Graficar(info_producto, producto):
+    ventas_producto = agrupar_ventas(info_producto)
     fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(range(len(ventas_por_producto)), ventas_por_producto['Unidades_vendidas'], label=producto)
-    agregar_tendencia(ax, ventas_por_producto)
-    configurar_grafico(ax, ventas_por_producto)
+    ax.plot(range(len(ventas_producto)), ventas_producto['Unidades_vendidas'], label=producto)
+    agregar_tendencia(ax, ventas_producto)
+    configurar_grafico(ax, ventas_producto)
     return fig
 
-def agrupar_ventas(datos_producto):
-    return datos_producto.groupby(['Año', 'Mes'])['Unidades_vendidas'].sum().reset_index()
+def agrupar_ventas(info_producto):
+    return info_producto.groupby(['Año', 'Mes'])['Unidades_vendidas'].sum().reset_index()
 
-def agregar_tendencia(ax, ventas_por_producto):
-    x = np.arange(len(ventas_por_producto))
-    y = ventas_por_producto['Unidades_vendidas']
+def agregar_tendencia(ax, ventas_producto):
+    x = np.arange(len(ventas_producto))
+    y = ventas_producto['Unidades_vendidas']
     z = np.polyfit(x, y, 1)
     p = np.poly1d(z)
     ax.plot(x, p(x), linestyle='--', color='red', label='Tendencia')
 
-def configurar_grafico(ax, ventas_por_producto):
+def configurar_grafico(ax, ventas_producto):
     ax.set_title('Evolución de Ventas Mensual')
     ax.set_xlabel('Año-Mes')
-    ax.set_xticks(range(len(ventas_por_producto)))
-    etiquetas = [f"{row.Año}" if row.Mes == 1 else "" for row in ventas_por_producto.itertuples()]
+    ax.set_xticks(range(len(ventas_producto)))
+    etiquetas = [f"{row.Año}" if row.Mes == 1 else "" for row in ventas_producto.itertuples()]
     ax.set_xticklabels(etiquetas)
     ax.set_ylabel('Unidades Vendidas')
     ax.set_ylim(0, None)
     ax.legend(title='Producto')
     ax.grid(True)
 
-def calcular_precio_promedio(datos_producto):
-    datos_producto['Precio_promedio'] = datos_producto['Ingreso_total'] / datos_producto['Unidades_vendidas']
-    precio_promedio = datos_producto['Precio_promedio'].mean()
-    precio_promedio_anual = datos_producto.groupby('Año')['Precio_promedio'].mean()
+def calcular_precio_promedio(info_producto):
+    info_producto['Precio_promedio'] = info_producto['Ingreso_total'] / info_producto['Unidades_vendidas']
+    precio_promedio = info_producto['Precio_promedio'].mean()
+    precio_promedio_anual = info_producto.groupby('Año')['Precio_promedio'].mean()
     variacion_precio_promedio_anual = precio_promedio_anual.pct_change().mean() * 100
     return precio_promedio, variacion_precio_promedio_anual
 
-def calcular_margen_promedio(datos_producto):
-    datos_producto['Ganancia'] = datos_producto['Ingreso_total'] - datos_producto['Costo_total']
-    datos_producto['Margen'] = (datos_producto['Ganancia'] / datos_producto['Ingreso_total']) * 100
-    margen_promedio = datos_producto['Margen'].mean()
-    margen_promedio_anual = datos_producto.groupby('Año')['Margen'].mean()
+def calcular_margen_promedio(info_producto):
+    info_producto['Ganancia'] = info_producto['Ingreso_total'] - info_producto['Costo_total']
+    info_producto['Margen'] = (info_producto['Ganancia'] / info_producto['Ingreso_total']) * 100
+    margen_promedio = info_producto['Margen'].mean()
+    margen_promedio_anual = info_producto.groupby('Año')['Margen'].mean()
     variacion_margen_promedio_anual = margen_promedio_anual.pct_change().mean() * 100
     return margen_promedio, variacion_margen_promedio_anual
 
-def calcular_unidades_vendidas(datos_producto):
-    unidades_vendidas = datos_producto['Unidades_vendidas'].sum()
-    unidades_por_año = datos_producto.groupby('Año')['Unidades_vendidas'].sum()
+def calcular_unidades_vendidas(info_producto):
+    unidades_vendidas = info_producto['Unidades_vendidas'].sum()
+    unidades_por_año = info_producto.groupby('Año')['Unidades_vendidas'].sum()
     variacion_anual_unidades = unidades_por_año.pct_change().mean() * 100
     return unidades_vendidas, variacion_anual_unidades
 
@@ -77,16 +77,16 @@ if archivo_cargado is not None:
         
         with st.container(border=True):
             st.subheader(f"{producto}")
-            datos_producto = datos[datos['Producto'] == producto]
-            precio_promedio, variacion_precio_promedio_anual = calcular_precio_promedio(datos_producto)
-            margen_promedio, variacion_margen_promedio_anual = calcular_margen_promedio(datos_producto)
-            unidades_vendidas, variacion_anual_unidades = calcular_unidades_vendidas(datos_producto)
+            info_producto = datos[datos['Producto'] == producto]
+            precio_promedio, variacion_precio_promedio_anual = calcular_precio_promedio(info_producto)
+            margen_promedio, variacion_margen_promedio_anual = calcular_margen_promedio(info_producto)
+            unidades_vendidas, variacion_anual_unidades = calcular_unidades_vendidas(info_producto)
             
             col1, col2 = st.columns([0.25, 0.75])
             with col1:
                 mostrar_metricas(col1, precio_promedio, variacion_precio_promedio_anual, margen_promedio, variacion_margen_promedio_anual, unidades_vendidas, variacion_anual_unidades)
             with col2:
-                fig = Venta_Graficar(datos_producto, producto)
+                fig = Venta_Graficar(info_producto, producto)
                 st.pyplot(fig)
 else:
     st.subheader("Por favor, sube un archivo CSV desde la barra lateral.")
